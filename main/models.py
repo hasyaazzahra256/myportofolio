3e5df79037fccc14da50af313c9624b3b6f6a7d3
Hasya <hasyazahra25@gmail.com>
2026-09-14 23:00:16 +0700

    Change start_date and ended_at to CharField

--- main/models.py
+++ main/models.py
@@ -15,8 +15,9 @@ class Experience(models.Model):
     title = models.CharField(max_length=255)
     description = models.TextField()
     thumbnail = models.URLField(blank=True, null=True)
-    start_date = models.DateField(null=True, blank=True)
-    ended_at = models.DateTimeField(blank=True, null=True)
+    start_date = models.CharField(max_length=50, blank=True, null=True)
+    ended_at = models.CharField(max_length=50, blank=True, null=True)
+    is_ongoing = models.BooleanField(default=False)
 
     def __str__(self):
         return self.title

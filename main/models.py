c01a0a7300258f2c3e210a5befacd401764bb3a1
Hasya <hasyazahra25@gmail.com>
2026-09-14 22:57:06 +0700

    Update Experience model and migration

--- main/models.py
+++ main/models.py
@@ -14,9 +14,8 @@ class Experience(models.Model):
     id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
     title = models.CharField(max_length=255)
     description = models.TextField()
-    category = models.CharField(max_length=20, choices=EXPERIENCE_CHOICES, default='full-time')
     thumbnail = models.URLField(blank=True, null=True)
-    started_at = models.DateTimeField(auto_now_add=True)
+    start_date = models.DateField(null=True, blank=True)
     ended_at = models.DateTimeField(blank=True, null=True)
 
     def __str__(self):

b2ca7985b2fc1b6dfe701c7113a5e361331e95e1
Hasya <hasyazahra25@gmail.com>
2026-09-20 19:20:30 +0700

    feat: configure URL patterns for experience views and API

--- main/urls.py
+++ main/urls.py
@@ -1,14 +1,29 @@
 from django.urls import path
-from main.views import show_main, show_experience, show_project, create_project, delete_project, get_projects_json, create_admin_pws
+from main.views import (
+    show_main, show_experience, create_experience, edit_experience, delete_experience, get_experiences_json,
+    show_project, create_project, edit_project, delete_project, get_projects_json,
+    create_admin_pws
+)
 
-app_name = 'main'
+app_name = "main"
 
 urlpatterns = [
-    path('', show_main, name='show_main'),
-    path('experience/', show_experience, name='show_experience'),
-    path('projects/', show_project, name='show_project'),
+    path("", show_main, name="show_main"),
+    
+    # experience URLs
+    path("experience/", show_experience, name="show_experience"),
+    path("experience/add/", create_experience, name="create_experience"),
+    path("experience/<uuid:id>/edit/", edit_experience, name="edit_experience"), # Gunakan <int:id> jika primary key bertipe integer
+    path("experience/<uuid:id>/delete/", delete_experience, name="delete_experience"),
+    path("api/experiences/", get_experiences_json, name="get_experiences_json"),
+
+    # project URLs
+    path("projects/", show_project, name="show_project"),
     path("projects/add/", create_project, name="create_project"),
-    path("projects/delete/<uuid:id>/", delete_project, name="delete_project"),
+    path("projects/<uuid:id>/edit/", edit_project, name="edit_project"),
+    path("projects/<uuid:id>/delete/", delete_project, name="delete_project"),
     path("api/projects/", get_projects_json, name="get_projects_json"),
-    path('create-admin-pws/', create_admin_pws, name='create_admin_pws'),
+
+    # admin PWS Helper
+    path("create-admin-pws/", create_admin_pws, name="create_admin_pws"),
 ]
